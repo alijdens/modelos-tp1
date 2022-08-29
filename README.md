f3b5bc6b8575442f67ce391610d2e085c0f7feaf
Axel Lijdens <axel.lijdens@wildlifestudios.com>
2022-08-28 22:06:04 -0300

    Entrega 1: algortimo greedy.

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
-# modelos-tp1
+# TP1
+
+## Impresión del problema
+
+El problema a simple vista parece complicado: para encontrar una solución óptima sería necesario probar todas las combinaciones de subconjuntos formados por las prendas y finalmente quedarse con la configuración que resulte en el menor tiempo. Esto significa que si hay `n` prendas, habrían `2^n` subconjuntos para probar. Claramente una solución de este estilo sería intratable para valores relativamente bajos de `n`.
+En cierto aspecto me recuerda al problema de la mochila: hay que acomodar elementos en conjuntos de forma de lograr el mejor resultado. Pero por otro lado, sería resolver varios de estos al mismo tiempo (ya que debemos armar la cantidad necesaria de mochilas). Si bien no puedo demostrarlo, _creo_ que es un problema NP-hard.
+
+# Resolución
+
+## Algortimo greedy
+
+El problema es complejo debido a las restricciones: si no hubiera ninguna, la solución sería trivial (un único lavado con todas las prendas). La primera idea que se me ocurre entonces es implementar un algritmo greedy el cual tomaría las prendas con mayor tiempo de lavado primero y luego tratar de agregar las demás en los grupos existentes. De esta forma estaría asignando la mayoría de las prendas a lavados donde estén las más pesadas.
+
+Es claro que este algoritmo no necesariamente resultaría en una solución óptima, ya que podría armarse una configuración distinta que reduzca la cantidad de lavados totales.
+
+## Otras ideas
+
+Una idea alternativa era escribir el problema en una forma recursiva para poder utilizar programción dinámica. Se podrían generar todos los subconjuntos válidos (es decir, los cuales incluyen prendas que pueden pertencer a un mismo lavado) ya que cuantas más restricciones haya, menos de estos deberían existir, y luego tratar de construir lavados que contengan todas las prendas combinándolos. Es claro que habrá combinaciones que se prueben múltiples veces, con lo que podríamos memorizar esos resultados intermedios para acelerar los cálculos. No llegué a probar esta alternativa por cuestiones de tiempo.
+
+# Comentarios finales de la entrega
+
+El algoritmo greedy parece tener un buen desempeño. Intenté mejorar el resultado manualmente pero me fue difícil encontrar un mejor (aunque realizar este trabajo manualmente es complicado, ja!) y su ejecución es bastante rápida.
---
+++ tp1.py
@@ -0,0 +1,121 @@
+import math
+from typing import Dict, List, Set
+from collections import defaultdict
+
+
+def parse_file(file_name:str):
+    with open(file_name) as fp:
+        data = fp.read()
+
+    lines = data.strip().split('\n')
+    lines = map(lambda line: line.strip(), lines)
+
+    # n = prendas
+    # m = incompatiblidades
+    n, m = None, None
+    edges, times = defaultdict(set), {}
+    for line in lines:
+        if line.startswith('c '):
+            # skip comment
+            continue
+
+        if line.startswith('p'):
+            # problem definition
+            _, _, n, m = line.split()
+        elif line.startswith('e'):
+            # incompatibilidad
+            _, id1, id2 = line.split()
+            edges[id1].add(id2)
+            edges[id2].add(id1)
+        elif line.startswith('n'):
+            # tiempos de lavado
+            _, id, duration = line.split()
+            times[id] = int(duration)
+        else:
+            assert False, f'Unexpected line type "{line}"'
+
+    # safety check
+    total_edges = sum(len(e) for e in edges.values())
+    assert total_edges == int(m), f'{total_edges=} != {int(m)=}'
+    assert len(times) == int(n), f'{len(times)=} != {int(n)=}'
+
+    return edges, times
+
+
+def solve_greedy(edges: Dict[str, set], times: Dict[str, int]):
+    def can_insert(item: str, set: set):
+        for incompatible in edges[item]:
+            if incompatible in set:
+                return False
+        return True
+
+    # ordeno las prendas por su duración (de mayor a menor)
+    by_weight = sorted(times, key=lambda k: -times[k])
+
+    # mapa en el que la clave es el tiempo del lavado y los valores son listas
+    # de lavados con ese tiempo
+    # por ejemplo, {3: [{1, 2}]} significa que hay un lavado con las prendas "1" y "2"
+    # que tiene una duración calculada de "3"
+    sets: Dict[List[set]] = defaultdict(list)
+    for item in by_weight:
+        t = times[item]
+
+        target_set_key = None
+
+        # intento asignar la prenda en algún lavado existente, empezando por los de mayor duración
+        for set_t in sorted(sets, reverse=True):
+            set_list = sets[set_t]
+            for i, s in enumerate(set_list):
+                if can_insert(item, s):
+                    target_set_key = (set_t, i)
+                    break
+            if target_set_key is not None:
+                break
+
+        if target_set_key is None:
+            # no se pudo encontrar un lavado compatible, se crea uno nuevo
+            sets[t].append({item})
+        else:
+            set_t, set_id = target_set_key
+
+            # agrego la prenda al lavado
+            s = sets[set_t][set_id]
+            s.add(item)
+            if t > set_t:
+                # la prenda tiene un tiempo mayor a todas las otras
+                # se actualiza el tiempo de lavado
+                del sets[set_t][set_id]
+                sets[t].append(s)
+
+    # armo la solución final recolectando todos los lavados
+    solution = []
+    for set_list in sets.values():
+        solution += set_list
+
+    return solution
+
+
+def write_output(solution: List[set], output_file: str):
+    with open(output_file, 'wt') as fp:
+        for i, items in enumerate(solution, start=1):
+            for item in items:
+                fp.write(f'{item} {i}\n')
+
+
+
+def print_stats(solution: List[Set[str]], times: Dict[str, int]):
+    durations = [max(times[c] for c in s) for s in solution]
+    for i, s in enumerate(solution):
+        print(f' - Lavado {i+1:2d} ({durations[i]:2d}):', ', '.join(s))
+    print('total', sum(durations))
+
+
+if __name__ == '__main__':
+    import sys
+    assert len(sys.argv) == 3, '  Usage: ./tp1 INPUT_FILE OUTPUT_FILE'
+
+    edges, times = parse_file(file_name=sys.argv[1])
+    solution = solve_greedy(edges=edges, times=times)
+
+    print_stats(solution, times)
+    write_output(solution=solution, output_file=sys.argv[2])
